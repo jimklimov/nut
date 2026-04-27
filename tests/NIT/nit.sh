8f7401d1c2973b782ade52bbdb50e42ba8e4145e
Jim Klimov <jimklimov+nut@gmail.com>
2026-04-27 02:50:08 +0200

    tests/NIT/nit.sh: fix duplicate (merge conflict in test branch)
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -3373,15 +3373,6 @@ setenv_ssl_perl() {
             ;;
     esac
 
-    case "${NUT_CAPATH}" in
-        ?":\\"*)
-            # Perl uses a platform-dependent PATH separator,
-            # however in mingw/msys2 is uses ":" which clashes
-            # with "C:\..." that Python insists on in this var.
-            _NUT_CAPATH="`realpath \"${NUT_CAPATH}\"`" && [ -n "${_NUT_CAPATH}" ] && NUT_CAPATH="${_NUT_CAPATH}" || true
-            ;;
-    esac
-
     if isTestablePerl && [ -n "${PERL}" ] ; then
         $PERL -e "use IO::Socket::SSL;" || {
             log_warn "The perl interpreter '$PERL' can not use IO::Socket::SSL module, so we will not FORCESSL in the test"
