b3519fd51998db6d730c207842d1563601ca041c
Jim Klimov <jimklimov+nut@gmail.com>
2026-04-27 02:49:30 +0200

    tests/NIT/nit.sh: fix NUT_CAPATH for PERL SSL tests on Windows (MSYS2) with forward slashes too [#1711]
    Signed-off-by: Jim Klimov <jimklimov+nut@gmail.com>

--- tests/NIT/nit.sh
+++ tests/NIT/nit.sh
@@ -3365,7 +3365,7 @@ setenv_ssl_perl() {
     setenv_ssl_common "perl"
 
     case "${NUT_CAPATH}" in
-        ?":\\"*)
+        ?":\\"*|?":/"*)
             # Perl uses a platform-dependent PATH separator,
             # however in mingw/msys2 is uses ":" which clashes
             # with "C:\..." that Python insists on in this var.
